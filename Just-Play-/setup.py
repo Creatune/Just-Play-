c2b23c2e45ebc3e5a3bf46fe36ad95e2fbdd4b74
Rishikesh Joshi <44409391+Creatune@users.noreply.github.com>
2019-06-11 06:04:39 -0700

    Add files via upload

---
+++ Just-Play-/setup.py
@@ -0,0 +1,20 @@
+import time
+import os
+import tkinter as tk
+from tkinter import messagebox
+
+def message_box(subject, content):                                                                            
+    root = tk.Tk()                                                                                           
+    root.attributes("-topmost", True)                                                                        
+    root.withdraw()                                                                                          
+    messagebox.showinfo(subject, content)                                                                    
+    try:                                                                                                     
+        root.destroy()                                                                                       
+    except:                                                                                                  
+        pass                                                                                                              
+message_box('Disclaimer!', 'This program WILL install a third party software... Terminate it if you do not want to install it')                                                                                                               
+message_box('','The download will start in exactly  4.5 SECONDS')                                                                                                                                                           
+time.sleep(4.5)
+os.system('sudo apt-get install python3-pip')
+os.system('pip3 install pygame')
+message_box('Done!','The installation process has completed. You can now run the game.py file and play the full game.')
